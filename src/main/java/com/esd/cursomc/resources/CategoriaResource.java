b6d860b9c7804691c81812a146f6ac80d6544057
Elton Dornelas <eltondornelas@gmail.com>
2019-09-24 18:16:50 -0300

    Criando repository e service para Categoria

--- src/main/java/com/esd/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/esd/cursomc/resources/CategoriaResource.java
@@ -1,13 +1,14 @@
 package com.esd.cursomc.resources;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.esd.cursomc.domain.Categoria;
+import com.esd.cursomc.services.CategoriaService;
 
 //Resource (recurso) é o nome padrão para os controladores REST, vem após o nome da classe domínio
 
@@ -15,17 +16,18 @@ import com.esd.cursomc.domain.Categoria;
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 
-	@RequestMapping(method=RequestMethod.GET) //atribuindo verbo HTTP "get"
-	public List<Categoria> listar() {
+	@Autowired
+	private CategoriaService service;
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.GET) //atribuindo verbo HTTP "get"
+	public ResponseEntity<?> find(@PathVariable Integer id) { //o PathVariable diz que o {id} da URL vai para o id da função
+		//Response Entity é um tipo especial do Spring que já armazena várias informações de uma resposta HTTP para um serviço REST
+		//? porque ele pode ser de qualquer tipo e que pode encontrar ou não.
 		
-		Categoria cat1 = new Categoria(1, "Informática");
-		Categoria cat2 = new Categoria(2, "Escritório");
-		
-		List<Categoria> lista = new ArrayList<>();
-		lista.add(cat1);
-		lista.add(cat2);
-		
-		return lista;
+		Categoria obj = service.buscar(id);
+		return ResponseEntity.ok().body(obj);
+
+		//nessa aula 14, vamos criar manualmente o banco de dados no H2
 	}
 	
 	
