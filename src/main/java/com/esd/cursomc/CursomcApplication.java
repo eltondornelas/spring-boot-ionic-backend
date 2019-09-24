317036bd2a019e75a838cfad5d0d0b7a23d192aa
Elton Dornelas <eltondornelas@gmail.com>
2019-09-24 20:47:37 -0300

    Criando operacao de instanciacao

--- src/main/java/com/esd/cursomc/CursomcApplication.java
+++ src/main/java/com/esd/cursomc/CursomcApplication.java
@@ -1,13 +1,35 @@
 package com.esd.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import com.esd.cursomc.domain.Categoria;
+import com.esd.cursomc.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		//esse comando já inicializa os dados no banco
+				
+		Categoria cat1 = new Categoria(null, "Informática"); //nulo para deixar o banco instanciar automaticamente
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+	}
+	
+	
 
 }
