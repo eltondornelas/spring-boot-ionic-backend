236ea7cbc964cf820bf8b84f7c7549084e3fed16
Elton Dornelas <eltondornelas@gmail.com>
2019-09-27 00:57:05 -0300

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/com/esd/cursomc/resources/ClienteResource.java
@@ -0,0 +1,34 @@
+package com.esd.cursomc.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.esd.cursomc.domain.Cliente;
+import com.esd.cursomc.services.ClienteService;
+
+//Resource (recurso) é o nome padrão para os controladores REST, vem após o nome da classe domínio
+
+@RestController
+@RequestMapping(value="/clientes")
+public class ClienteResource {
+
+	@Autowired
+	private ClienteService service;
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.GET) //atribuindo verbo HTTP "get"
+	public ResponseEntity<?> find(@PathVariable Integer id) { //o PathVariable diz que o {id} da URL vai para o id da função
+		//Response Entity é um tipo especial do Spring que já armazena várias informações de uma resposta HTTP para um serviço REST
+		//? porque ele pode ser de qualquer tipo e que pode encontrar ou não.
+		
+		Cliente obj = service.buscar(id);
+		return ResponseEntity.ok().body(obj);
+
+		//nessa aula 14, vamos criar manualmente o banco de dados no H2
+	}
+	
+	
+}
