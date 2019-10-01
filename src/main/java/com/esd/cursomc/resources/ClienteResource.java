59998d5a78a1d512db4ec5d3efb8a7a77775718a
Elton Dornelas <eltondornelas@gmail.com>
2019-10-01 18:25:25 -0300

    PUT, DELETE e GET para Cliente

--- src/main/java/com/esd/cursomc/resources/ClienteResource.java
+++ src/main/java/com/esd/cursomc/resources/ClienteResource.java
@@ -1,13 +1,23 @@
 package com.esd.cursomc.resources;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.esd.cursomc.domain.Cliente;
+import com.esd.cursomc.domain.Cliente;
+import com.esd.cursomc.dto.ClienteDTO;
 import com.esd.cursomc.services.ClienteService;
 
 //Resource (recurso) é o nome padrão para os controladores REST, vem após o nome da classe domínio
@@ -30,5 +40,47 @@ public class ClienteResource {
 		//nessa aula 14, vamos criar manualmente o banco de dados no H2
 	}
 	
+	@RequestMapping(value="/{id}", method = RequestMethod.PUT) //PUT para atualizar
+	public ResponseEntity<Void> update(@Valid @RequestBody ClienteDTO objDto, @PathVariable Integer id) {
+		Cliente obj = service.fromDTO(objDto);
+		obj.setId(id);
+		obj = service.update(obj);
+		
+		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.DELETE) //atribuindo verbo HTTP "get"
+	public ResponseEntity<Void> delete(@PathVariable Integer id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(method=RequestMethod.GET) //como queremos que ele list todas as categorias, não vamos colocar o value id
+	public ResponseEntity<List<ClienteDTO>> findAll() {
+				
+		List<Cliente> list = service.findAll();
+		List<ClienteDTO> listDto = list.stream().map(obj -> new ClienteDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
+		
+		//perceba que dessa forma quando colocamos no Postman o get categorias ele vem acoplado com seus produtos e não queremos isso
+		//para corrigir e controlar para que venha apenas as categrias vamos utilizar DTO (Data Transfer Object) ou projeção de dados, controlar os dados que você quiser
+		//Aula 34 bem importante sobre isso.
+	}
+	
+	@RequestMapping(value = "/page", method=RequestMethod.GET) //concatena o /page com o /categoria lá em cima da classe
+	public ResponseEntity<Page<ClienteDTO>> findPage(
+			@RequestParam(value="page", defaultValue = "0") Integer page,
+			@RequestParam(value="linesPerPage", defaultValue = "24")Integer linesPerPage,
+			@RequestParam(value="orderBy", defaultValue = "nome")String orderBy,
+			@RequestParam(value="direction", defaultValue = "ASC")String direction) {
+
+		//página default 0 é a primeira página. Linhas por página é interessante o número 24, porque é múltiplo de 1, 2, 3 e 4...
+		//Aula 35
+		
+		Page<Cliente> list = service.findPage(page, linesPerPage, orderBy, direction);
+		Page<ClienteDTO> listDto = list.map(obj -> new ClienteDTO(obj));
+		return ResponseEntity.ok().body(listDto);
+		
+	}	
 	
 }
