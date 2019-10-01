bd51bf17070cd7b785c517f038762c3d6af22aac
Elton Dornelas <eltondornelas@gmail.com>
2019-09-30 23:25:42 -0300

    Paginacao com parametros opcionais na requisicao

--- src/main/java/com/esd/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/esd/cursomc/resources/CategoriaResource.java
@@ -5,11 +5,13 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -69,5 +71,22 @@ public class CategoriaResource {
 		
 		//perceba que dessa forma quando colocamos no Postman o get categorias ele vem acoplado com seus produtos e não queremos isso
 		//para corrigir e controlar para que venha apenas as categrias vamos utilizar DTO (Data Transfer Object) ou projeção de dados, controlar os dados que você quiser
+		//Aula 34 bem importante sobre isso.
+	}
+	
+	@RequestMapping(value = "/page", method=RequestMethod.GET) //concatena o /page com o /categoria lá em cima da classe
+	public ResponseEntity<Page<CategoriaDTO>> findPage(
+			@RequestParam(value="page", defaultValue = "0") Integer page,
+			@RequestParam(value="linesPerPage", defaultValue = "24")Integer linesPerPage,
+			@RequestParam(value="orderBy", defaultValue = "nome")String orderBy,
+			@RequestParam(value="direction", defaultValue = "ASC")String direction) {
+
+		//página default 0 é a primeira página. Linhas por página é interessante o número 24, porque é múltiplo de 1, 2, 3 e 4...
+		//Aula 35
+		
+		Page<Categoria> list = service.findPage(page, linesPerPage, orderBy, direction);
+		Page<CategoriaDTO> listDto = list.map(obj -> new CategoriaDTO(obj));
+		return ResponseEntity.ok().body(listDto);
+		
 	}
 }
