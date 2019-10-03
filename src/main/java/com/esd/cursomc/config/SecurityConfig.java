fc7d4dc6ce6dc852a8bb3834a6ee25e9501a8d69
Elton Dornelas <eltondornelas@gmail.com>
2019-10-03 19:55:38 -0300

    Salvando perfis de usuario na base de dados

--- src/main/java/com/esd/cursomc/config/SecurityConfig.java
+++ src/main/java/com/esd/cursomc/config/SecurityConfig.java
@@ -31,7 +31,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	private static final String[] PUBLIC_MATCHERS_GET = {
 			"/produtos/**",
-			"/categorias/**"
+			"/categorias/**",
+			"/clientes/**"
 	}; //esse caminho com GET é apenas para Leitura, não pode ser modificado
 
 	@Override
--- src/main/java/com/esd/cursomc/domain/Cliente.java
+++ src/main/java/com/esd/cursomc/domain/Cliente.java
@@ -5,17 +5,20 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import javax.persistence.CascadeType;
 import javax.persistence.CollectionTable;
 import javax.persistence.Column;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
+import com.esd.cursomc.domain.enums.Perfil;
 import com.esd.cursomc.domain.enums.TipoCliente;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
@@ -45,12 +48,17 @@ public class Cliente implements Serializable {
 	private Set<String> telefones = new HashSet<>();
 	//por telefone ser algo muito simples, apenas tem 1 string, não vamos criar como atributo e sim como uma coleção do tipo Set (não aceita repetição)
 	
+	@ElementCollection(fetch=FetchType.EAGER)
+	@CollectionTable(name="PERFIS") //nome da tabela será PERFIS
+	private Set<Integer> perfis = new HashSet<>();
+	
 	@JsonIgnore //pedidos de clientes não serão serializados
 	@OneToMany(mappedBy = "cliente")
 	private List<Pedido> pedidos = new ArrayList<>();
 	
 	public Cliente() {
-		
+		addPerfil(Perfil.CLIENTE);
+		//na regra de negócio ficou padronizado que todo usuário é um Cliente, só alguns selecionados serão Admin.
 	}
 
 	public Cliente(Integer id, String nome, String email, String cpfOuCnpj, TipoCliente tipo, String senha) {
@@ -61,6 +69,7 @@ public class Cliente implements Serializable {
 		this.cpfOuCnpj = cpfOuCnpj;
 		this.tipo = (tipo == null) ? null : tipo.getCod();
 		this.senha = senha;
+		addPerfil(Perfil.CLIENTE);
 	}
 
 	public Integer getId() {
@@ -109,7 +118,15 @@ public class Cliente implements Serializable {
 
 	public void setSenha(String senha) {
 		this.senha = senha;
-	}	
+	}
+	
+	public Set<Perfil> getPerfis() {
+		return perfis.stream().map(x -> Perfil.toEnum(x)).collect(Collectors.toSet());
+	}
+
+	public void addPerfil(Perfil perfil) {
+		perfis.add(perfil.getCod());
+	}
 
 	public List<Endereco> getEnderecos() {
 		return enderecos;
