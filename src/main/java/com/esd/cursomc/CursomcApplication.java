1b0258642491673653825f326d0afe5225bc88b5
Elton Dornelas <eltondornelas@gmail.com>
2019-10-04 11:07:54 -0300

    Salvando primeiro arquivo no S3

--- src/main/java/com/esd/cursomc/CursomcApplication.java
+++ src/main/java/com/esd/cursomc/CursomcApplication.java
@@ -1,12 +1,18 @@
 package com.esd.cursomc;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.esd.cursomc.services.S3Service;
+
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
 
+	@Autowired
+	private S3Service s3Service;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
@@ -16,6 +22,7 @@ public class CursomcApplication implements CommandLineRunner {
 		// esse comando já inicializa os dados no banco
 		//tu que estava nesse arquivo foi para o DBService() que é chamado pelo TestConfig
 
+		s3Service.uploadFile("C:\\temp\\teste.jpg");
 	}
 
 }
